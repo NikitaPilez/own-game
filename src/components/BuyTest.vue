3a905d2c5975d7bd4e1bec360db64aaf705955f4
Nikita Pilets <pileznikita99@gmail.com>
2024-06-07 12:02:46 +0300

    Buy product

--- src/components/BuyTest.vue
+++ src/components/BuyTest.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
     <h1>Tests</h1>
-    <div v-for="test in tests" :key="test.id" class="test">
-      <h2>{{ test.name }}</h2>
-      <p>Price: {{ test.price }} USDT</p>
-      <button @click="buyTest(test)">Buy</button>
-      <div v-if="selectedTest && selectedTest.id === test.id">
-        <img :src="testQrCode" alt="QR Code">
+    <div class="tests-grid">
+      <div v-for="test in tests" :key="test.id" class="test">
+        <h2>{{ test.title }}</h2>
+        <p>Price: {{ test.price }} USDT</p>
+        <button
+            :class="test.is_active ? 'btn-green' : 'btn-blue'"
+            @click="test.is_active ? playTest(test) : buyTest(test)"
+        >
+          {{ test.is_active ? 'Play' : 'Buy' }}
+        </button>
+        <div v-if="selectedTest && selectedTest.id === test.id">
+          <img :src="testQrCode" alt="QR Code">
+        </div>
       </div>
     </div>
-    <div v-if="loading">Processing payment...</div>
-    <div v-if="success">Payment successful! You now have access to the test.</div>
-    <div v-if="error">{{ error }}</div>
   </div>
 </template>
 
@@ -19,42 +23,55 @@
 import QRCode from 'qrcode';
 import TonWeb from 'tonweb';
 import axios from 'axios';
-import { API_BASE_URL } from '@/config';
+import { API_BASE_URL, TELEGRAM_ID, RECIPIENT_ADDRESS } from '@/config';
+import { inject } from 'vue';
 
 export default {
   name: "BuyTest",
   data() {
     return {
       tests: [],
-      loading: false,
-      success: false,
-      error: null,
-      recipientAddress: '0QDD5-7iLDPvJ_T_LKwlkXfyexl2iL7v9-BH-_a3vJ25Vjf0',
+      recipientAddress: RECIPIENT_ADDRESS,
       selectedTest: null,
       testQrCode: '',
+      interval: null,
+      telegramUserId: null,
     };
   },
-  created() {
+  mounted() {
+    this.getTelegramUserId();
     this.fetchTests();
+    this.interval = setInterval(this.fetchTests, 3000);
   },
   methods: {
-    async fetchTests() {
-      try {
-        const response = await axios.get(`${API_BASE_URL}/api/packages`);
-        this.tests = response.data;
-      } catch (error) {
-        console.error('Error fetching tests:', error);
+    getTelegramUserId() {
+      const tg = inject('telegram');
+
+      if (tg && tg.initDataUnsafe.user) {
+        this.telegramUserId = tg.initDataUnsafe.user.id;
+      } else {
+        this.telegramUserId = TELEGRAM_ID;
       }
     },
+    fetchTests() {
+      axios.get(`${API_BASE_URL}/api/packages`, {
+        params: {
+          telegramUserId: this.telegramUserId,
+        }
+      }).then(response => {
+        this.tests = response.data;
+      }).catch(error => {
+        console.error(error);
+      });
+    },
+    async playTest(test) {
+      console.log(test);
+    },
     async buyTest(test) {
-      this.loading = true;
-      this.error = null;
-      this.success = false;
       try {
-
         const response = await axios.post(`${API_BASE_URL}/api/orders`, {
           packageId: test.id,
-          telegramUserId: 621085735,
+          telegramUserId: this.telegramUserId,
         });
 
         const order = response.data;
@@ -65,14 +82,8 @@ export default {
 
         this.testQrCode = await QRCode.toDataURL(paymentLink);
         this.selectedTest = test;
-
-        window.open(paymentLink, '_blank');
-
-        this.success = true;
       } catch (error) {
-        this.error = `Payment error: ${error.message}`;
-      } finally {
-        this.loading = false;
+        console.log(error.message);
       }
     }
   }
@@ -80,7 +91,27 @@ export default {
 </script>
 
 <style scoped>
+.tests-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+}
+
 .test {
-  margin-bottom: 20px;
+  background-color: #f9f9f9;
+  border: 1px solid #ddd;
+  padding: 20px;
+  border-radius: 8px;
+  text-align: center;
+}
+
+.btn-blue {
+  background-color: blue;
+  color: white;
+}
+
+.btn-green {
+  background-color: green;
+  color: white;
 }
 </style>
--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
 export const API_BASE_URL = process.env.VUE_APP_API_BASE_URL;
+export const TELEGRAM_ID = process.env.VUE_APP_TELEGRAM_ID;
+export const RECIPIENT_ADDRESS = process.env.VUE_APP_RECIPIENT_ADDRESS;
